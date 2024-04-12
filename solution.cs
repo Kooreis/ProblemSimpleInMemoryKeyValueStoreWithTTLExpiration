0c7c794843e8602f768e519ac2ec7ee1f80f5208
Kooreis <kooreis.official@gmail.com>
2024-04-12 17:55:30 -0500

    Added a 'set' method to the KeyValueStore class. This method sets a key-value pair in the store and starts a timeout to delete the key-value pair after the specified TTL (time to live). If the key already exists in the store, it clears the existing timeout before setting the new value and starting a new timeout.

--- solution.cs
+++ solution.cs
@@ -1,5 +1,11 @@
-class KeyValueStore {
-    constructor() {
-        this.store = {};
+set(key, value, ttl) {
+        if (this.store[key]) {
+            clearTimeout(this.store[key].timeoutId);
+        }
+
+        const timeoutId = setTimeout(() => {
+            delete this.store[key];
+        }, ttl);
+
+        this.store[key] = { value, timeoutId };
     }
-}
